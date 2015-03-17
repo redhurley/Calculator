f7d66f8ec308c98dd60fae71ab3cb100db4074c5
Donnie Wang <donniewang@gmail.com>
2015-03-16 21:15:38 -0700

    Setup buttons and append actions.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsCurrentlyTypingANumber: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle
+        if userIsCurrentlyTypingANumber {
+            display.text = display.text! + digit!
+        } else {
+            display.text = digit
+            userIsCurrentlyTypingANumber = true
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
